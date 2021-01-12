b1f4a986f16feffff364d9c38bdf7c0f78be44dd
Towsif-Kafi <37453829+Towsif12@users.noreply.github.com>
2021-01-13 05:55:23 +0600

    Update
    Updated Name , Server Error Handler, Footer CSS

--- js/servers.js
+++ js/servers.js
@@ -5,20 +5,29 @@ $(document).ready(function() {
         $.ajax({
             url: "https://api.mcsrvstat.us/2/play.pixeledgebd.com"
         }).then(function(data) {
-            //console.log(data.ip)
+            if (data.online == true) {
             $("#pixeledge").html(`<h2>PixelEdge: </h2><h3>${data.players.online}</h3>`);
+            } else {
+                $("#pixeledge").html(`<h2>PixelEdge: </h2><h3>Offline</h3>`);
+            }
         });
         $.ajax({
             url: "https://api.mcsrvstat.us/2/play.minecraftersbd.com"
         }).then(function(data) {
-            //console.log(data.ip)
-            $("#banglacraft").html(`<h2>BanglaCraft: </h2><h3>${data.players.online}</h3>`);
+            if (data.online == true) {
+                $("#banglacraft").html(`<h2>BanglaCraft: </h2><h3>${data.players.online}</h3>`);
+                } else {
+                    $("#banglacraft").html(`<h2>BanglaCraft: </h2><h3>Offline</h3>`);
+                }
         });
         $.ajax({
             url: "https://api.mcsrvstat.us/2/play.ultracraftbd.com"
         }).then(function(data) {
-            //console.log(data.ip)
-            $("#ultracraft").html(`<h2>UltraCraft: </h2><h3>${data.players.online}</h3>`);
+            if (data.online == true) {
+                $("#ultracraft").html(`<h2>UltraCraft: </h2><h3>${data.players.online}</h3>`);
+                } else {
+                    $("#ultracraft").html(`<h2>UltraCraft: </h2><h3>Offline</h3>`);
+                }
         });
     }, 5000)
 })
