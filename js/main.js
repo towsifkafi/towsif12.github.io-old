8e857afda05e6871afc4f2d58d860e7e88931029
Towsif-Kafi <37453829+Towsif12@users.noreply.github.com>
2021-01-14 05:39:35 +0600

    Updated Pages

--- js/main.js
+++ js/main.js
@@ -26,19 +26,3 @@ $(document).ready(function() {
     })
 })
 
-var speed = 'slow';
-
-$('html, body').hide();
-
-$(document).ready(function() {
-    $('html, body').fadeIn(speed, function() {
-        $('a[href], button[href]').click(function(event) {
-            var url = $(this).attr('href');
-            if (url.indexOf('#') == 0 || url.indexOf('javascript:') == 0) return;
-            event.preventDefault();
-            $('.notfound').fadeOut(speed, function() {
-                window.location = url;
-            });
-        });
-    });
-});
